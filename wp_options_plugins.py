1df48f5654d62fc9fe7c448e7d9ae9ff6f372264
Dave Hrynkiw <dave@solarbotics.com>
2020-04-14 21:55:41 -0600

    Able to print the plugin list to terminal; starting to config CLI options for it

--- wp_options_plugins.py
+++ wp_options_plugins.py
@@ -42,44 +42,39 @@ def get_cli_arguments():
                         default='wp_options.txt')
     parser.add_argument('-o', '--output', type=str, required=False, metavar='file',  help='WP file output file',
                         default='wp_options_new.txt')
+    parser.add_argument('-l', '--list', type=str, required=False, help='Display Plugins list')
+    parser.add_argument('-d', '--delete', type=str, required=False, help='Delete Plugins by number')
     return parser.parse_args()
 
 args = get_cli_arguments()
 
+# Process the input file
 for line in args.input:
-    print("Line: {}".format(line))
-#    print(line.split('{'))
+    print("Line: {}".format(line)) #Disable when done debugging
 
-    match = re.search(r"^a:.*{(.*)",line)
-    result = match.group(1).rstrip('; }')
+    match = re.search(r"^a:.*{(.*)",line)   # strip off the preamble "a:0{" from the datafield entry
+    result = match.group(1).rstrip('; }')   # strip off the matching end "}"
     #print("Result={}".format(result))
-    result = re.split('i:',result)
-    result.remove('')
+    result = re.split('i:',result)          # break down the link by the index value (which kills the value 'i:')
+    result.remove('')                       # delete the initial blank entry
     #print(result)
-    result = ["i:" + s for s in result]
+    result = ["i:" + s for s in result]     # add the stripped 'i:' back to the elements
     print(result)
-    for i in range(len(result)):
-        print("Element {}={}".format(i,result[i]))
-        plugin_name=(re.search(r'(?<=")(.*)(?=")', result[i]).group())    #.group() gets the value, span() gets the start, end of match
-        plugin_length=(re.search(r'(?<=")(.*)(?=")', result[i]).span())
-        plugin_length1 = plugin_length[1] - plugin_length[0]
-        print("name={}, span={}, len={}".format(plugin_name, plugin_length, plugin_length1))
+    # for i in range(len(result)):
+    #     print("Element {}={}".format(i,result[i]))
+    #     plugin_name=(re.search(r'(?<=")(.*)(?=")', result[i]).group())    #.group() gets the value, span() gets the start, end of match
+    #     plugin_length=(re.search(r'(?<=")(.*)(?=")', result[i]).span())
+    #     plugin_length1 = plugin_length[1] - plugin_length[0]
+    #     print("name={}, span={}, len={}".format(plugin_name,plugin_length, plugin_length1))
 
 
+def print_plugin_list(plugin_list):
+    for i,r in enumerate(plugin_list, 0):
+        plugin_name=(re.search(r'(?<=")(.*)(?=")', plugin_list[i]).group())      #.group() gets the plugin values
+        plugin_length=(re.search(r'(?<=")(.*)(?=")', plugin_list[i]).span())     #.span() gets start & end of match
+        plugin_length1 = plugin_length[1] - plugin_length[0]                # calculate length of plugin name (needed)
+        #print("name={}, span={}, len={}".format(plugin_name,plugin_length, plugin_length1))
+        print("{}: {}".format(i, plugin_name))   # Print an indexed list of plugins to choose from.
 
 
-#    print(result.split('";'))
-#    print("     {}".format(re.split('";',result)))
-#     delim='";'
-#     for x in result:
-#         s = [e+delim for e in x.split(delim) if e]
-#         print(s)
-
-    #print("     {}".format(re.split
-    #print("Regex:\n{}".format(match.group(1)))
-
-#print("input path: {0}".format(args.input))
-# for line in args.input:
-# #    print (line.strip())
-#     print(unserialize(line))
 
