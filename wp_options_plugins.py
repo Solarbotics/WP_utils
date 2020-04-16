50c7bc5dd2c5202a68344b6264753ea4c0066253
Dave Hrynkiw <dave@solarbotics.com>
2020-04-16 01:53:00 -0600

    Clean up debug crap

--- wp_options_plugins.py
+++ wp_options_plugins.py
@@ -17,15 +17,6 @@ as example) as:
     ;)
 Some help:
 https://wordpress.stackexchange.com/questions/45109/how-to-make-sense-of-the-active-plugins-option-value-to-enable-and-disable-certa
-"""
-"""
-ToDO:
-- Read in the list from saved file (copied from the database field)
-- Make it human-readable (I find a newline before each "a:[0-9]*.," regex does nicely)
-- Select which entry to disable
-- Write out the disabled entry to log file for later re-inclusion (even manually
-- re-write out a fresh list, with a:xx = new total number of plugins, and each list_element having a sequential "a:xx" element followed by the remaining datafields
-     This list then can be re-written to the database directly to disable the plugins in question.
 
 Spec: Dave Hrynkiw, April 11 2020
 """
@@ -51,7 +42,6 @@ def get_cli_arguments():
     delplugin_parser.add_argument('numbers', type=int, nargs='*')
     listplugin_parser = subparsers.add_parser('list')
     listplugin_parser.add_argument('vals', type=str, nargs='*')
-
     return parser.parse_args()
 
 
@@ -62,26 +52,15 @@ for line in args.input:
 
     match = re.search(r"^a:.*{(.*)", line)  # strip off the preamble "a:0{" from the datafield entry
     result = match.group(1).rstrip('; }')  # strip off the matching end "}"
-    # print("Result={}".format(result))
     result = re.split('i:', result)  # break down the link by the index value (which kills the value 'i:')
     result.remove('')  # delete the initial blank entry
-    # print(result)
     result = ["i:" + s for s in result]  # add the stripped 'i:' back to the elements
-    # print(result)
-    # for i in range(len(result)):
-    #     print("Element {}={}".format(i,result[i]))
-    #     plugin_name=(re.search(r'(?<=")(.*)(?=")', result[i]).group())    #.group() gets the value, span() gets the start, end of match
-    #     plugin_length=(re.search(r'(?<=")(.*)(?=")', result[i]).span())
-    #     plugin_length1 = plugin_length[1] - plugin_length[0]
-    #     print("name={}, span={}, len={}".format(plugin_name,plugin_length, plugin_length1))
-
 
 def print_plugin_list(plugin_list):
     for i, r in enumerate(plugin_list, 0):
         plugin_name = (re.search(r'(?<=")(.*)(?=")', plugin_list[i]).group())  # .group() gets the plugin values
         plugin_length = (re.search(r'(?<=")(.*)(?=")', plugin_list[i]).span())  # .span() gets start & end of match
         plugin_length1 = plugin_length[1] - plugin_length[0]  # calculate length of plugin name (needed)
-        # print("name={}, span={}, len={}".format(plugin_name,plugin_length, plugin_length1))
         print("{}: {}".format(i, plugin_name))  # Print an indexed list of plugins to choose from.
 
 
