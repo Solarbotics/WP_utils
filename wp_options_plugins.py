9714f9740e82c1459f0ad0dc4392393c5c7b28e2
Dave Hrynkiw <dave@solarbotics.com>
2020-04-16 00:26:54 -0600

    Can delete from plugins list Can create/write to deleted plugins file (datestamped lines)

--- wp_options_plugins.py
+++ wp_options_plugins.py
@@ -31,14 +31,18 @@ Spec: Dave Hrynkiw, April 11 2020
 """
 import argparse
 import re
+from datetime import datetime
+import os
+
 
 # get the defaults from command line
 def get_cli_arguments():
     parser = argparse.ArgumentParser(description='Wordpress wp_options manipulator')
 
-    parser.add_argument('-i', '--input', type=argparse.FileType('r'), required=False, metavar='file', help='WP file containing wp_options',
+    parser.add_argument('-i', '--input', type=argparse.FileType('r'), required=False, metavar='file',
+                        help='WP file containing wp_options',
                         default='wp_options.txt')
-    parser.add_argument('-o', '--output', type=str, required=False, metavar='file',  help='WP file output file',
+    parser.add_argument('-o', '--output', type=str, required=False, metavar='file', help='WP file output file',
                         default='wp_options_new.txt')
     parser.add_argument('-l', '--list', required=False, help='Display Plugins list', action='store_true')
 
@@ -47,19 +51,20 @@ def get_cli_arguments():
     delplugin_parser.add_argument('numbers', type=int, nargs='*')
     return parser.parse_args()
 
+
 args = get_cli_arguments()
 # Process the input file
 for line in args.input:
-    #print("Line: {}".format(line)) #Disable when done debugging
+    # print("Line: {}".format(line)) #Disable when done debugging
 
-    match = re.search(r"^a:.*{(.*)",line)   # strip off the preamble "a:0{" from the datafield entry
-    result = match.group(1).rstrip('; }')   # strip off the matching end "}"
-    #print("Result={}".format(result))
-    result = re.split('i:',result)          # break down the link by the index value (which kills the value 'i:')
-    result.remove('')                       # delete the initial blank entry
-    #print(result)
-    result = ["i:" + s for s in result]     # add the stripped 'i:' back to the elements
-    #print(result)
+    match = re.search(r"^a:.*{(.*)", line)  # strip off the preamble "a:0{" from the datafield entry
+    result = match.group(1).rstrip('; }')  # strip off the matching end "}"
+    # print("Result={}".format(result))
+    result = re.split('i:', result)  # break down the link by the index value (which kills the value 'i:')
+    result.remove('')  # delete the initial blank entry
+    # print(result)
+    result = ["i:" + s for s in result]  # add the stripped 'i:' back to the elements
+    # print(result)
     # for i in range(len(result)):
     #     print("Element {}={}".format(i,result[i]))
     #     plugin_name=(re.search(r'(?<=")(.*)(?=")', result[i]).group())    #.group() gets the value, span() gets the start, end of match
@@ -69,12 +74,13 @@ for line in args.input:
 
 
 def print_plugin_list(plugin_list):
-    for i,r in enumerate(plugin_list, 0):
-        plugin_name=(re.search(r'(?<=")(.*)(?=")', plugin_list[i]).group())      #.group() gets the plugin values
-        plugin_length=(re.search(r'(?<=")(.*)(?=")', plugin_list[i]).span())     #.span() gets start & end of match
-        plugin_length1 = plugin_length[1] - plugin_length[0]                # calculate length of plugin name (needed)
-        #print("name={}, span={}, len={}".format(plugin_name,plugin_length, plugin_length1))
-        print("{}: {}".format(i, plugin_name))   # Print an indexed list of plugins to choose from.
+    for i, r in enumerate(plugin_list, 0):
+        plugin_name = (re.search(r'(?<=")(.*)(?=")', plugin_list[i]).group())  # .group() gets the plugin values
+        plugin_length = (re.search(r'(?<=")(.*)(?=")', plugin_list[i]).span())  # .span() gets start & end of match
+        plugin_length1 = plugin_length[1] - plugin_length[0]  # calculate length of plugin name (needed)
+        # print("name={}, span={}, len={}".format(plugin_name,plugin_length, plugin_length1))
+        print("{}: {}".format(i, plugin_name))  # Print an indexed list of plugins to choose from.
+
 
 # If is a list request, just pump out the list with an index number
 if args.list:
@@ -83,10 +89,32 @@ if args.list:
 # If its a delete request, cycle through the list, and delete the lines by index number
 if args.func == 'del':
     numbers = args.numbers
+    print(datetime.now())
     for n in numbers:
         print(result[n])
-    ans=input("***Confirm***: Remove these plugins from the list (y/N)?: ")
+        print(args.input.name)
+    ans = input("***Confirm***: Remove these plugins from the list (y/N)?: ")
     if ans == 'Y' or ans == 'y':
         print("Confirmed")
+        # check for and create a backup file from the sourcefile name
+        if not os.path.exists(args.input.name + '.backup'):
+            pass
+
+        with open(args.input.name + '.backup', 'a') as backupfile:
+            backupdate = datetime.now()
+            for n in numbers:
+                print("{},{}".format(backupdate, result[n]))
+                print("{},{}".format(backupdate, result[n]), file=backupfile)
+            print("Deleted plugins backup data written")
+        print(numbers)
+        for m in numbers:
+            print("{}={}".format(m, result[m]))
+            del result[m]
+        # print_plugin_list(result)
+        # for i, r in enumerate(plugin_list, 0):
+        #     plugin_name = (re.search(r'(?<=")(.*)(?=")', plugin_list[i]).group())  # .group() gets the plugin values
+        #     plugin_length = (re.search(r'(?<=")(.*)(?=")', plugin_list[i]).span())  # .span() gets start & end of match
+        #     plugin_length1 = plugin_length[1] - plugin_length[0]  # calculate length of plugin name (needed)
+        #     if i in numbers:
     else:
         print("Aborted")
